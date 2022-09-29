10a8d886fceb947edb174932d07a73cd51d46ec7
felipelyra3 <felipelyra3@gmail.com>
2022-09-29 16:55:03 -0300

    post categories

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import pg from 'pg';
+import Joi from "joi";
 
 const { Pool } = pg;
 const connection = new Pool({
@@ -11,18 +12,32 @@ const connection = new Pool({
 });
 
 const server = express();
+server.use(express.json());
 
 ////////// Categories //////////
+const postCategories = Joi.object({
+    name: Joi.string().empty().required()
+});
+
 server.get('/categories', async (req, res) => {
     const categories = await connection.query(`SELECT * FROM categories;`);
     res.status(200).send(categories.rows);
 });
 
 server.post('/categories', async (req, res) => {
-
+    try {
+        await postCategories.validateAsync(req.body);
+        await connection.query(`INSERT INTO categories (name) values($1);`, [req.body.name]);
+        res.sendStatus(201);
+    } catch (error) {
+        res.status(422).send(error.message);
+    }
 });
 
 ////////// INTERNAL //////////
+server.get('/', async (req, res) => {
+    res.send(req.body);
+});
 
 ////////// Server listen //////////
 server.listen(4000, () => {
