eb99d8f8a7ecded4a2de1a985fd38ffa2cd5b012
felipelyra3 <felipelyra3@gmail.com>
2022-09-30 05:15:41 -0300

    put customers

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -3,6 +3,7 @@ import BaseJoi from 'joi';
 import JoiDate from '@joi/date';
 const Joi = BaseJoi.extend(JoiDate);
 
+//GetCustomers
 async function GetCustomers(req, res) {
     if (req.query.cpf) {
         const search = (await connection.query(`SELECT * FROM customers WHERE cpf ILIKE '${req.query.cpf}%';`)).rows;
@@ -18,6 +19,7 @@ async function GetCustomers(req, res) {
     }
 };
 
+//GetCustomersById
 async function GetCustomersById(req, res) {
     try {
         const customers = (await connection.query(`SELECT * FROM customers WHERE id = ${req.params.id};`)).rows;
@@ -33,7 +35,8 @@ async function GetCustomersById(req, res) {
     }
 };
 
-//Schema
+//PostCustomers
+//Schema PostCustomers
 const postCustomers = Joi.object({
     name: Joi.string().empty(),
     phone: Joi.string().regex(/^[0-9]+$/).min(10).max(11),
@@ -59,4 +62,36 @@ async function PostCustomers(req, res) {
     }
 };
 
-export { GetCustomers, GetCustomersById, PostCustomers };
+//PutCustomers
+//Schema
+const putCustomers = Joi.object({
+    name: Joi.string().empty(),
+    phone: Joi.string().regex(/^[0-9]+$/).min(10).max(11),
+    cpf: Joi.string().regex(/^[0-9]+$/).min(11).max(11),
+    birthday: Joi.date().format('YYYY-MM-DD').utc()
+});
+
+async function PutCustomers(req, res) {
+    try {
+        await putCustomers.validateAsync(req.body);
+        const nCustomers = (await connection.query(`SELECT COUNT(*) FROM customers;`)).rows;
+        const customers = (await connection.query(`SELECT * FROM customers;`)).rows;
+        for (let i = 0; i < nCustomers[0].count; i++) {
+            if (req.body.cpf === customers[i].cpf) {
+                res.sendStatus(409);
+                return;
+            }
+        }
+        await connection.query(`UPDATE customers SET name=$1, phone=$2, cpf=$3, birthday=$4 WHERE id = $5;`, [req.body.name, req.body.phone, req.body.cpf, req.body.birthday, req.params.id]);
+        res.sendStatus(200);
+    } catch (error) {
+        res.status(422).send(error.message);
+    }
+};
+
+//PutCustomers with no id
+async function PutCustomersWithNoId(req, res) {
+    res.sendStatus(404);
+}
+
+export { GetCustomers, GetCustomersById, PostCustomers, PutCustomers, PutCustomersWithNoId };
--- src/routers/customers.routes.js
+++ src/routers/customers.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { GetCustomers, GetCustomersById, PostCustomers } from "../controllers/customers.controllers.js";
+import { GetCustomers, GetCustomersById, PostCustomers, PutCustomers, PutCustomersWithNoId } from "../controllers/customers.controllers.js";
 
 const router = express.Router();
 
 router.get('/customers', GetCustomers);
 router.get('/customers/:id', GetCustomersById);
 router.post('/customers', PostCustomers);
+router.put('/customers/:id', PutCustomers);
+router.put('/customers', PutCustomersWithNoId);
 
 
 export default router;
