796b29021d922f6a70062b911085bd78f4ac96fb
felipelyra3 <felipelyra3@gmail.com>
2022-09-30 04:41:28 -0300

    post customers

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -1,5 +1,7 @@
 import connection from '../database/db.js';
-import Joi from "joi";
+import BaseJoi from 'joi';
+import JoiDate from '@joi/date';
+const Joi = BaseJoi.extend(JoiDate);
 
 async function GetCustomers(req, res) {
     if (req.query.cpf) {
@@ -31,4 +33,30 @@ async function GetCustomersById(req, res) {
     }
 };
 
-export { GetCustomers, GetCustomersById };
+//Schema
+const postCustomers = Joi.object({
+    name: Joi.string().empty(),
+    phone: Joi.string().regex(/^[0-9]+$/).min(10).max(11),
+    cpf: Joi.string().regex(/^[0-9]+$/).min(11).max(11),
+    birthday: Joi.date().format('YYYY-MM-DD').utc()
+});
+
+async function PostCustomers(req, res) {
+    try {
+        await postCustomers.validateAsync(req.body);
+        const nCustomers = (await connection.query(`SELECT COUNT(*) FROM customers;`)).rows;
+        const customers = (await connection.query(`SELECT * FROM customers;`)).rows;
+        for (let i = 0; i < nCustomers[0].count; i++) {
+            if (req.body.cpf === customers[i].cpf) {
+                res.sendStatus(409);
+                return;
+            }
+        }
+        await connection.query(`INSERT INTO customers (name, phone, cpf, birthday) values($1, $2, $3, $4)`, [req.body.name, req.body.phone, req.body.cpf, req.body.birthday]);
+        res.sendStatus(201);
+    } catch (error) {
+        res.status(422).send(error.message);
+    }
+};
+
+export { GetCustomers, GetCustomersById, PostCustomers };
--- src/routers/customers.routes.js
+++ src/routers/customers.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { GetCustomers, GetCustomersById } from "../controllers/customers.controllers.js";
+import { GetCustomers, GetCustomersById, PostCustomers } from "../controllers/customers.controllers.js";
 
 const router = express.Router();
 
 router.get('/customers', GetCustomers);
 router.get('/customers/:id', GetCustomersById);
+router.post('/customers', PostCustomers);
 
 
 export default router;
